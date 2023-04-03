9e32540fc4f979ab1e55cbd99644a022c5549794
Mike Weaver <michael@triplewhale.com>
2023-04-03 11:38:45 -0400

    Add react - add typing for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,9 @@
-import { Button as MantineButton } from "@mantine/core";
+import React from "react";
+import { Button as MantineButton, ButtonProps } from "@mantine/core";
 
-export const Button = ({ ...props }: any) => {
+interface BaseButtonProps extends ButtonProps {}
+
+export const Button: React.FC = ({ ...props }: ButtonProps) => {
   console.log("----------------------");
   console.log("TW Custom Button Used!");
   console.log("Props", props);
