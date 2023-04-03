be6532ea438d5980d73c2ed7ae75c90552b9eaaa
Mike Weaver <michael@triplewhale.com>
2023-04-03 11:43:39 -0400

    Example additional props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Button as MantineButton, ButtonProps } from "@mantine/core";
 
-interface BaseButtonProps extends ButtonProps {}
+interface TWButtonProps extends ButtonProps {
+  analyticsLabel?: string;
+}
 
-export const Button: React.FC = ({ ...props }: ButtonProps) => {
+export const Button: React.FC = ({ ...props }: TWButtonProps) => {
   console.log("----------------------");
   console.log("TW Custom Button Used!");
   console.log("Props", props);
   console.log("----------------------");
+
+  if (props.analyticsLabel) {
+    console.log("Analytics Label", props.analyticsLabel);
+  }
 
   return <MantineButton {...props} />;
 };
