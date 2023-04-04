9e0012aeeec58c9a30fb9f6e0b5854984a2c3c48
Mike Weaver <michael@triplewhale.com>
2023-04-04 09:31:45 -0400

    Run build

--- dist/components/ComponentProvider.js
+++ dist/components/ComponentProvider.js
@@ -2,9 +2,9 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import React from "react";
 import { MantineProvider } from "@mantine/core";
 export const ComponentProvider = ({ ...props }) => {
-    return (_jsx(MantineProvider, { withNormalizeCSS: true, withGlobalStyles: true, theme: {
+    return (_jsx(MantineProvider, { withNormalizeCSS: true, withGlobalStyles: true, ...props, theme: {
             ...props.theme,
             /** Put your mantine theme override here */
-        }, ...props }));
+        } }));
 };
 //# sourceMappingURL=ComponentProvider.js.map
