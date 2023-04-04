44e6f7b8d7e587d1e1015a578be83f426b1f533f
Mike Weaver <michael@triplewhale.com>
2023-04-04 11:07:35 -0400

    Bump + build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
-export * from "@mantine/core";
-export * from "@mantine/hooks";
-export * from "@mantine/carousel";
+import * as Core_1 from "@mantine/core";
+export { Core_1 as Core };
+import * as Hooks_1 from "@mantine/hooks";
+export { Hooks_1 as Hooks };
+import * as Carousel_1 from "@mantine/carousel";
+export { Carousel_1 as Carousel };
 export { Button } from "./components/Button";
 export { ComponentProvider } from "./components/ComponentProvider";
 //# sourceMappingURL=index.js.map
