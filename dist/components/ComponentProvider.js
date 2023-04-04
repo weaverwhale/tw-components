4604d07aba302c78952376cd44b890def7c75212
Mike Weaver <michael@triplewhale.com>
2023-04-04 10:19:50 -0400

    Bump + build

--- dist/components/ComponentProvider.js
+++ dist/components/ComponentProvider.js
@@ -5,6 +5,6 @@ export const ComponentProvider = ({ ...props }) => {
     return (_jsx(MantineProvider, { withNormalizeCSS: true, withGlobalStyles: true, ...props, theme: {
             ...props.theme,
             /** Put your mantine theme override here */
-        } }));
+        }, children: props.children }));
 };
 //# sourceMappingURL=ComponentProvider.js.map
