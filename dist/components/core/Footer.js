c826ae25d2bf1e14593a5fe88e3aedd74ac0bde6
Mike Weaver <michael@triplewhale.com>
2023-04-05 18:43:10 -0400

    Footer updates

--- dist/components/core/Footer.js
+++ dist/components/core/Footer.js
@@ -1,6 +1,7 @@
-import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { Text, Flex, Button } from "@mantine/core";
 export function Footer() {
     const today = new Date();
-    return (_jsx("footer", { children: _jsxs("p", { children: [_jsxs("span", { children: ["\u00A9 ", today.getFullYear(), " Triple Whale. All rights reserved."] }), _jsx("br", {}), _jsx("small", { children: _jsx("a", { href: "/reports/lighthouse.html", target: "_blank", children: "View Lighthouse Report" }) })] }) }));
+    return (_jsxs("footer", { children: [_jsxs(Flex, { gap: "sm", justify: { base: "center" }, align: "flex-start", direction: "row", wrap: "wrap", mb: 20, children: [_jsx(Button, { component: "a", radius: "xl", color: "dark", size: "xs", compact: true, href: "https://github.com/weaverwhale/tw-components-playground", target: "_blank", rel: "noopener noreferrer", children: "Github" }), _jsx(Button, { component: "a", radius: "xl", color: "red", size: "xs", compact: true, href: "https://www.npmjs.com/package/@weaverwhale/tw-components", target: "_blank", rel: "noopener noreferrer", children: "NPM Package" }), _jsx(Button, { component: "a", radius: "xl", color: "blue", size: "xs", compact: true, href: "/reports/lighthouse.html", target: "_blank", rel: "noopener noreferrer", children: "Lighthouse Report" })] }), _jsxs(Text, { align: "center", children: ["\u00A9 ", today.getFullYear(), " Triple Whale. All rights reserved."] })] }));
 }
 //# sourceMappingURL=Footer.js.map
--- src/components/core/Footer.tsx
+++ src/components/core/Footer.tsx
@@ -1,18 +1,57 @@
+import { Text, Flex, Button } from "@mantine/core";
+
 export function Footer() {
   const today = new Date();
   return (
     <footer>
-      <p>
-        <span>
-          &copy; {today.getFullYear()} Triple Whale. All rights reserved.
-        </span>
-        <br />
-        <small>
-          <a href="/reports/lighthouse.html" target="_blank">
-            View Lighthouse Report
-          </a>
-        </small>
-      </p>
+      <Flex
+        gap="sm"
+        justify={{ base: "center" }}
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+        mb={20}
+      >
+        <Button
+          component="a"
+          radius="xl"
+          color="dark"
+          size="xs"
+          compact
+          href="https://github.com/weaverwhale/tw-components-playground"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </Button>
+        <Button
+          component="a"
+          radius="xl"
+          color="red"
+          size="xs"
+          compact
+          href="https://www.npmjs.com/package/@weaverwhale/tw-components"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          NPM Package
+        </Button>
+        <Button
+          component="a"
+          radius="xl"
+          color="blue"
+          size="xs"
+          compact
+          href="/reports/lighthouse.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lighthouse Report
+        </Button>
+      </Flex>
+      <Text align={"center"}>
+        &copy; {today.getFullYear()} Triple Whale. All rights reserved.
+      </Text>
     </footer>
   );
 }
